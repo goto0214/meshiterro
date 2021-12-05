9639995e1c9c9687bee238f8adbc42249f77fdab
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-05 10:28:38 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '999be8716b857bc8db3d3c1d422451de999a23a88024bef0e250d9d904f77765b8b41f7417d694afd449ad88de4127f2b13c265d446af919c9cf84ce406e3d8f'
